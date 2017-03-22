d6245156e06d03984e4bff1e73b89150d5f5599e
Nino Gonzales <wanjak.gonzales@gmail.com>
2017-03-22 02:36:12 -0700

    Added Menu Screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void StoreBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void OptionsBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void CreditsBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
