04a0489d94ba11be77b309164932d4a0b6a40240
aputri <av_ashfan_12@yahoo.com>
2017-03-27 16:34:12 -0700

    altered location of spawn, decreased spawn time

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,14 +5,14 @@ namespace CompleteProject
     public class EnemyManager : MonoBehaviour
     {
         public GameObject enemy;                // The enemy prefab to be spawned.
-        public float spawnTime = 6f;            // How long between each spawn.
-		public Transform[] spawnPoints = new Transform[3];         // An array of the spawn points this enemy can spawn from.
+        public float spawnTime = 5f;            // How long between each spawn.
+		public Transform[] spawnPoints = new Transform[5];         // An array of the spawn points this enemy can spawn from.
 
 
         void Start ()
         {
             // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-            InvokeRepeating ("Spawn", spawnTime, spawnTime);
+            InvokeRepeating ("Spawn", 0f, spawnTime);
         }
 
 
