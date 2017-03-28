d8ab422b4ace2d808da5188e2c1793068cc5c7c1
Nino Gonzales <wanjak.gonzales@gmail.com>
2017-03-27 22:18:29 -0700

    Added ammo manager. Added pause button and reload button

---
+++ Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoManager : MonoBehaviour {
+
+    PlayerShooting playerShooting;
+    GameObject gun;
+    Text text;
+
+	void Awake () {
+        gun = GameObject.FindGameObjectWithTag("weapon");
+        playerShooting = gun.GetComponent<PlayerShooting>();
+        text = GetComponent<Text>();
+    }
+	
+	void Update () {
+        text.text = " x" + playerShooting.bulletInMagazine + " ∞";
+    }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    PlayerShooting playerShooting;
+    GameObject gun;
+    Animation reload;
+
+    void Awake()
+    {
+        gun = GameObject.FindGameObjectWithTag("weapon");
+        playerShooting = gun.GetComponent<PlayerShooting>();
+        reload = gun.GetComponent<Animation>();
+    }
+
+    public void settingsBtn()
+    {   
+        //pauses game and mute sounds. Still need to add Menu
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+        }
+    }
+
+    public void reloadBtn()
+    {
+        playerShooting.bulletInMagazine = playerShooting.magazineSize;
+        reload.Play("reload");
+    }
+	
+}
