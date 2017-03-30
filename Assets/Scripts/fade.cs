ce969bb6bc10ec922ea00258b4a52f04fb4476b4
juliuswuwu <aapple1994@gmail.com>
2017-03-30 02:33:16 -0700

    logo scene

---
+++ Assets/Scripts/fade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class fade : MonoBehaviour {
+	public Image fadeimage;
+	public string loadLevel;
+
+	IEnumerator Start(){
+		fadeimage.canvasRenderer.SetAlpha(0.0f);
+
+		FadeIn();
+		yield return new WaitForSeconds(2.5f);
+		FadeOut();
+		yield return new WaitForSeconds (2.5f);
+		SceneManager.LoadScene(loadLevel);
+	}
+
+
+	void FadeIn(){
+		fadeimage.CrossFadeAlpha (1.0f, 1.5f, false);
+
+	}
+	void FadeOut(){
+		fadeimage.CrossFadeAlpha (0.0f, 2.5f, false);
+
+	}
+}
